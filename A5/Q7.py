8207a531569730f48b8cd813ebb16ea10aef8904
Ashishkc22 <ashishchoudhari224@gmail.com>
2024-10-19 10:53:37 +0530

    first commit

--- A5/Q7.py
+++ A5/Q7.py
@@ -4,12 +4,13 @@ def word_frequency():
     with open('input.txt', 'r') as infile:
         text = infile.read()
         words = text.split()
-        
     word_count = Counter(words)
-    
+    print(word_count)
     # Sort by frequency in descending order
     sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
     
     print("Word Frequency:")
     for word, count in sorted_word_count:
         print(f"{word}: {count}")
+
+word_frequency()
